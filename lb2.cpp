9cbbce44e19ad1a963aec7b51b3136da7ba23a2f
vacuumcleaner228 <79622368+vacuumcleaner228@users.noreply.github.com>
2021-03-06 19:33:33 +0300

    Add files via upload

---
+++ lb2.cpp
@@ -0,0 +1,22 @@
+﻿
+#include <iostream>
+
+using namespace std;
+int main()
+{
+    setlocale(LC_ALL, "RUS");
+    double n=0;
+    double factorial=1;
+    cout << "Введите число n: ";
+    cin >> n;
+    if (n == 0) {
+        cout << "Факториал n равен: 1";
+        return 0;
+    }
+    do {
+        factorial *= n;
+        n -= 2;
+    } while (n > 1);
+        cout << "Факториал n равен: " << factorial;
+    return 0;
+}
