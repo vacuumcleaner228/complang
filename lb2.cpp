e8833e872f1eaed3e7e993c868b02f7123a3f81c
vacuumcleaner228 <79622368+vacuumcleaner228@users.noreply.github.com>
2021-03-08 15:22:45 +0300

    Add files via upload

--- lb2.cpp
+++ lb2.cpp
@@ -1,14 +1,37 @@
-﻿
-#include <iostream>
+﻿#include <iostream>
 
 using namespace std;
+
+double getValue()
+{
+    while (true) 
+    {
+        cout << "Введите число n: ";
+        double n;
+        cin >> n;
+        if ((cin.fail()) || (n<0))
+        {
+            if (cin.fail())
+            {
+                cin.clear();
+                cin.ignore(32767, '\n');
+                cout << "n - число. \n";
+            }
+            else cout << "n не может быть меньше 0. \n";
+        }
+        else
+        {
+            cin.ignore(32767, '\n');
+            return n;
+        }
+    }
+}
+
 int main()
 {
     setlocale(LC_ALL, "RUS");
-    double n=0;
-    double factorial=1;
-    cout << "Введите число n: ";
-    cin >> n;
+    double factorial = 1;
+    double n = getValue();
     if (n == 0) {
         cout << "Факториал n равен: 1";
         return 0;
@@ -17,6 +40,6 @@ int main()
         factorial *= n;
         n -= 2;
     } while (n > 1);
-        cout << "Факториал n равен: " << factorial;
+    cout << "Факториал n равен: " << factorial;
     return 0;
 }
