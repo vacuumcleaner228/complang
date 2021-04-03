81b5b867ff6f4f4628c34fa68033c0eb02fa07f4
vacuumcleaner228 <79622368+vacuumcleaner228@users.noreply.github.com>
2021-04-03 11:37:52 +0300

    Add files via upload

---
+++ lb7.cpp
@@ -0,0 +1,70 @@
+﻿
+#include <iostream>
+using namespace std;
+
+class Myclass
+{
+public:
+    long int result;
+    long int factorial;
+
+    void getFactorial()
+    {
+        if (factorial == 0)
+        {
+            cout << "Факториал n равен: 1" << endl;
+        }
+        else
+        {
+            do
+            {
+                result *= factorial;
+                factorial -= 2;
+            } while (factorial > 1);
+
+            cout << "Факториал n равен: " << result;
+        }
+    }
+    
+};
+
+long int getValue()
+{
+    while (true)
+    {
+        cout << "Введите число n: ";
+        long int n;
+        cin >> n;
+        if ((cin.fail()) || (n < 0))
+        {
+            if (cin.fail())
+            {
+                cin.clear();
+                cin.ignore(32767, '\n');
+                cout << "n - число. \n";
+            }
+            else
+            {
+                cout << "n не может быть меньше 0. \n";
+            }
+        }
+        else
+        {
+            cin.ignore(32767, '\n');
+            return n;
+        }
+    }
+}
+
+int main()
+{   
+    setlocale(LC_ALL, "RUS");
+
+    Myclass n;
+
+    n.factorial = getValue();
+    n.result = 1;
+    n.getFactorial();
+
+    return 0;
+}
